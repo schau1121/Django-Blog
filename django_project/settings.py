a3292db5e10fe1c729b4a91a38bd58a2335dfa7e
schau1121 <simon.chau1121@gmail.com>
2021-04-29 23:18:45 -0700

    Updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_BLOG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_BLOG_DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'schau1121-django-blog.herokuapp.com',
